67e4ce202850aadcee3e49878f8648e2226a527e
al <alxayeed@gmail.com>
2021-01-26 12:19:23 +0600

    serlizer added for Article

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Article
+
+
+
+class ArticleSerializer(serializers.Serializer):
+    ''' Serializer for  Article Model'''
+
+    title = serializers.CharField(max_length=100)
+    description = serializers.CharField(max_length=500)
+
+    def create(self, validated_data):
+        return Article.objects.create(validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get(
+            'description', instance.description)
