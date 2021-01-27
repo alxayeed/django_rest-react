4af6573ceb55a2d99b73f7d618cf514a1cfe5252
al <alxayeed@gmail.com>
2021-01-27 19:14:27 +0600

    viewset updated to ModelViewSet

--- api/views.py
+++ api/views.py
@@ -14,38 +14,41 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 
 
-class ArticleViewSet(viewsets.ViewSet):
+class ArticleViewSet(viewsets.ModelViewSet):
 
-    def list(self, requeset):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    def create(self, request):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def list(self, requeset):
+    #     articles = Article.objects.all()
+    #     serializer = ArticleSerializer(articles, many=True)
+    #     return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        queryset = Article.objects.all()
-        article = get_object_or_404(queryset, pk=pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+    # def create(self, request):
+    #     serializer = ArticleSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def update(self, request, pk=None):
-        article = Article.objects.get(pk=pk)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+    # def retrieve(self, request, pk=None):
+    #     queryset = Article.objects.all()
+    #     article = get_object_or_404(queryset, pk=pk)
+    #     serializer = ArticleSerializer(article)
+    #     return Response(serializer.data)
 
-    def destroy(self, request, pk=None):
-        article = Article.objects.get(pk=pk)
-        article.delete()
-        return Response('Item Deleted', status=status.HTTP_404_NOT_FOUND)
+    # def update(self, request, pk=None):
+    #     article = Article.objects.get(pk=pk)
+    #     serializer = ArticleSerializer(article, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors)
+
+    # def destroy(self, request, pk=None):
+    #     article = Article.objects.get(pk=pk)
+    #     article.delete()
+    #     return Response('Item Deleted', status=status.HTTP_404_NOT_FOUND)
 
 
 """
