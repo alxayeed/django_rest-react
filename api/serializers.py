6c26cc3ea04942ebb9f63a472f78dff036046847
al <alxayeed@gmail.com>
2021-01-27 20:39:47 +0600

    User list will not return password-extra kwargs added to UserSerializer.

--- api/serializers.py
+++ api/serializers.py
@@ -17,3 +17,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
+
+        extra_kwargs = {'password': {
+            'write_only': True,
+            'required': True
+        }}
