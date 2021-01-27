28ce8606cb93fbd6d3ebfccfad34b1c6611b56c2
al <alxayeed@gmail.com>
2021-01-27 21:13:52 +0600

    authentication working

--- APIProject/settings.py
+++ APIProject/settings.py
@@ -94,11 +94,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticated',
-#     ]
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 
 # Internationalization
