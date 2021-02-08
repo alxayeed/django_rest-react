079c04cfed7f4e4ab41f56bd6576bdc5db34b7ca
alxayeed <alxayeed@gmail.com>
2021-02-08 19:11:11 +0600

    Login form created

--- my_app/src/components/LoginForm.js
+++ my_app/src/components/LoginForm.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function LoginForm() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const loginBtn = () => {
+        console.log('Login button clicked')
+    }
     return (
-        <div>
-            <h1>Please login</h1>
+        <div className="container App">
+            <h1>Please Login</h1>
+            <br /><br />
+            <label htmlFor="username" className="form-label" id="username" >Username</label>
+            <input type="text" className="form-control" placeholder="Enter your username" value={username} onChange={e => setUsername(e.target.value)} />
+
+            <label htmlFor="password" className="form-label" id="password">Password</label>
+            <input type="password" className="form-control" placeholder="Enter your password" value={password} onChange={e => setPassword(e.target.value)} />
+            <br />
+            <button onClick={() => loginBtn()} className="btn btn-success" style={{ width: '100%' }}>Login</button>
+
         </div>
     )
 }
